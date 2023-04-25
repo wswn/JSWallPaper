c4c5cceda57becb22ed386c905f38aacc2c95c8d
Junying <junyinghu@umass.edu>
2023-04-25 14:50:46 -0400

    Clean JS code.

--- scripts/wdbyte_script.js
+++ scripts/wdbyte_script.js
@@ -7,31 +7,33 @@ const isWindows = navigator.platform.startsWith('Win');
 const pathSep = isWindows ? '\\' : '/';
 
 downloadLinks.forEach(function(link) {
-  var button = document.createElement('button');
-  button.innerHTML = 'Set Background';
-  button.style.marginLeft = '10px';
-  button.style.fontSize = (parseFloat(window.getComputedStyle(link).fontSize) / 4 * 3) + 'px';
-  // link.style.fontSize
-  button.addEventListener('click', function() {
-    fileName = link.parentNode.childNodes[0].textContent.trim() + '.jpg';
-    new QWebChannel(qt.webChannelTransport, function(channel) {
-        const regex_href = /:\/\/|\./g;
-        var folder = self.location.href.replace(regex_href, "_")
-        if (!folder.endsWith('/')) {
-            folder += '/'
-        }
-        const regex_ym = /\d{4}-\d{2}/;
-        const match = fileName.match(regex_ym);
-        const year_mon = match ? match[0] : '';
-        console.log(year_mon)
-        if (!folder.includes(year_mon)) {
-            folder += year_mon + '/'
-        }
-        // Reformat path with current platform separator
-        const regex_pathSep = /\//g;
-        let fullPath = (folder+fileName).replace(regex_pathSep, pathSep)
-        channel.objects.handler.set_background(link.href, fullPath);
+    var button = document.createElement('button');
+    button.innerHTML = 'Set Background';
+    button.style.marginLeft = '10px';
+    button.style.fontSize = (parseFloat(window.getComputedStyle(link).fontSize) / 4 * 3) + 'px';
+
+    // Onclick callback.
+    button.addEventListener('click', function() {
+        // Transform the url as a valid directory.
+        const regexHref = /:\/\/|\./g;
+        let folder = self.location.href.replace(regexHref, "_");
+        folder += folder.endsWith('/')? '' : '/';
+
+        // Add date as sub-directory.
+        let fileName = link.parentNode.childNodes[0].textContent.trim() + '.jpg';
+        const regexDate = /\d{4}-\d{2}/;
+        const match = fileName.match(regexDate);
+        const date = match ? match[0] : '';
+        folder += folder.includes(date)? '' : date + '/';
+
+        // Reformat path with the separator of current platform.
+        const regexPathSep = /\//g;
+        let fullPath = (folder+fileName).replace(regexPathSep, pathSep)
+
+        // API: Set Desktop Background.
+        new QWebChannel(qt.webChannelTransport, function(channel) {
+            channel.objects.handler.set_background(link.href, fullPath);
+        });
     });
-  });
-  link.parentNode.insertBefore(button, link.nextSibling);
+    link.parentNode.insertBefore(button, link.nextSibling);
 });
